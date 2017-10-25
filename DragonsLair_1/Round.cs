9e5c9e1c36eb20cd61cd7615bcc4849956d15bdc
Jacob Søfeldt Vilhelmsen <jaco3154@edu.eal.dk>
2017-10-25 12:42:41 +0200

    Alle test gennemført med forklaringer

--- DragonsLair_1/Round.cs
+++ DragonsLair_1/Round.cs
@@ -33,14 +33,38 @@ namespace DragonsLair_1
 
         public List<Team> GetWinningTeams()
         {
-            // TODO: Implement this method
-            return null;
+            //Der laves en ny liste over de eksisterende teams som indeholder de vindende hold
+            List<Team> winningTeams = new List<Team>();
+            //Hver kamp gennemgås i et loop hvorefter vinderen (x) tilføjes til winningTeams listen. Vi kender vinder fra Tournament classen.
+            foreach (Match x in matches)
+            {
+                //Her tilføjes det vindende hold til listen
+                winningTeams.Add(x.Winner);
+            }
+
+            return winningTeams;
         }
 
         public List<Team> GetLosingTeams()
         {
-            // TODO: Implement this method
-            return null;
+            //Der laves en liste over de tabende hold
+            List<Team> losingTeams = new List<Team>();
+            //Hver kamp gennemgås
+            foreach (Match y in matches)
+            {
+                //Navnene sammenlignes mellem firstopponent og vinderen.
+                if (y.FirstOpponent.Name == y.Winner.Name)
+                {
+                    //Er navnet på vinderen matchet af navnet på first opponent, vinder firstoppnent
+                    losingTeams.Add(y.SecondOpponent);
+                }
+                else
+                {
+                    //Ellers vinder secondopponent
+                    losingTeams.Add(y.FirstOpponent);
+                }
+            }
+            return losingTeams;
         }
     }
 }
